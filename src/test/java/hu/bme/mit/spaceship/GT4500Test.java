0676000463c596adb11a418d6e915abe79e9369a
futobarna <futo1998@gmail.com>
2019-05-17 02:08:43 -0700

    Mock test

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -1,23 +1,30 @@
 package hu.bme.mit.spaceship;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-public class GT4500Test {
+import org.junit.Before;
+import org.junit.Test;
 
+public class GT4500Test {
+  private TorpedoStore mockTS1;
+  private TorpedoStore mockTS2;
   private GT4500 ship;
 
-  @BeforeEach
+  @Before
   public void init(){
-    this.ship = new GT4500();
+    mockTS1 = mock(TorpedoStore.class);
+    mockTS2 = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockTS1, mockTS2);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mockTS1.isEmpty()).thenReturn(false);
+    when(mockTS2.isEmpty()).thenReturn(false);
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
